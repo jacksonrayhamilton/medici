50cb02f7d343e37258b3fe309bf809a8321212e1
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2019-11-23 11:59:28 -0800

    Scaffold main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let coins = 1692;
+    let money = 100.54;
+    print_inputs(coins, money);
+    let quarters = 0;
+    let dimes = 0;
+    let nickels = 0;
+    let pennies = 0;
+    print_result(quarters, dimes, nickels, pennies);
+}
+
+fn print_inputs(coins: u32, money: f64) {
+    println!(concat!(
+        "Coins: {}\n",
+        "Money: ${}"
+    ), coins, money);
+}
+
+fn print_result(quarters: u32, dimes: u32, nickels: u32, pennies: u32) {
+    println!(concat!(
+        "Quarters: {}\n",
+        "Dimes: {}\n",
+        "Nickels: {}\n",
+        "Pennies: {}"
+    ), quarters, dimes, nickels, pennies);
 }
