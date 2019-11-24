d17e559059db15899a49abd8519d1fb370376b84
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2019-11-23 17:41:45 -0800

    Refactor printing and result checking

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ fn main() {
     let dimes = 0;
     let nickels = 0;
     let pennies = 0;
+    println!();
     print_result(quarters, dimes, nickels, pennies);
+    check_result(coins, money, quarters, dimes, nickels, pennies);
 }
 
 fn print_inputs(coins: u32, money: u32) {
@@ -23,20 +25,34 @@ fn print_inputs(coins: u32, money: u32) {
 }
 
 fn print_result(quarters: u32, dimes: u32, nickels: u32, pennies: u32) {
-    let coins = quarters + dimes + nickels + pennies;
-    let money = sum_of_denominations(quarters, dimes, nickels, pennies);
     println!(concat!(
-        "\n",
         "RESULT\n",
         "Quarters: {}\n",
         "Dimes: {}\n",
         "Nickels: {}\n",
-        "Pennies: {}\n",
-        "\n",
-        "CHECK\n",
-        "Coins: {}\n",
-        "Money: {}"
-    ), quarters, dimes, nickels, pennies, coins, format_money(money));
+        "Pennies: {}"
+    ), quarters, dimes, nickels, pennies);
+}
+
+fn check_result(expected_coins: u32, expected_money: u32,
+                quarters: u32, dimes: u32, nickels: u32, pennies: u32) {
+    let calculated_coins = quarters + dimes + nickels + pennies;
+    let calculated_money = sum_of_denominations(quarters, dimes, nickels, pennies);
+    println!();
+    print_check(expected_coins, expected_money, calculated_coins, calculated_money);
+}
+
+fn print_check(expected_coins: u32, expected_money: u32,
+               calculated_coins: u32, calculated_money: u32) {
+    println!(
+        concat!(
+            "CHECK\n",
+            "Coins: {} ({})\n",
+            "Money: {} ({})"
+        ),
+        calculated_coins, format_success(expected_coins == calculated_coins),
+        format_money(calculated_money), format_success(expected_money == calculated_money)
+    );
 }
 
 fn sum_of_denominations(quarters: u32, dimes: u32, nickels: u32, pennies: u32) -> u32 {
@@ -50,4 +66,8 @@ fn format_money(money: u32) -> String {
     let dollars = money / 100;
     let cents = money % 100;
     format!("${}.{:02}", dollars, cents)
+}
+
+fn format_success(success: bool) -> String {
+    if success { "✓" } else { "✗" }.to_string()
 }
