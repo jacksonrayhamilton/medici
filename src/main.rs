dc6e126aac1f5964f5ba403b1bb78cb169e3ee81
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2019-11-23 21:44:50 -0800

    Move calculation logic in main to its own function

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,15 @@ fn main() {
     let coins = 100;
     let money = 1200;
     printer::print_inputs(coins, money);
+    let (quarters, dimes, nickels, pennies) = calculate_result(coins, money);
+    check_result(coins, money, quarters, dimes, nickels, pennies);
+}
+
+fn calculate_result(coins: u32, money: u32) -> (u32, u32, u32, u32) {
     let (quarters, dimes, nickels, pennies) = solver::calculate_result(coins, money);
     println!();
     printer::print_result(quarters, dimes, nickels, pennies);
-    check_result(coins, money, quarters, dimes, nickels, pennies);
+    (quarters, dimes, nickels, pennies)
 }
 
 fn check_result(expected_coins: u32, expected_money: u32,
